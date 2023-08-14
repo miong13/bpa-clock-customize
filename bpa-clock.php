a2bf7509f1b436a41b821897f17096c766ff14cb
Mark Ramos <miong13@gmail.com>
2023-08-14 15:00:59 +0800

    first commit

---
+++ bpa-clock.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Plugin Name:       Best Price Arts (Clock Customizer)
+ * Plugin URI:        
+ * Description:       Custom WooCommerce Product Filtering
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Mark Lyndon Ramos
+ * Author URI:        https://markramosonline.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://markramosonline.com/atf-woofilter/
+ * Text Domain:       atf-woofilter
+ * Domain Path:       /languages
+ */
